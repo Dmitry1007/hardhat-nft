3d4d7140741e4dadb138fe3f1982eed79a4385a6
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-07 12:26:37 -0600

    Mint token to message sender

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -4,5 +4,19 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("Dogie", "DOG") {}
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mint() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+        return s_tokenCounter;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
