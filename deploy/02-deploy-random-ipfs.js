61105f6bfec2d5df68cc81ca5c4e135d06a7b3bf
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-10 11:02:08 -0600

    Hard coded token uri's

--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -5,4 +5,34 @@ const { verify } = require("../utils/verify")
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await deployments.get("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    let tokenUris = [
+        "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+        "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+        "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+    ]
+
+    log("----------------------------------------------------")
+    args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].callbackGasLimit,
+        tokenUris,
+        networkConfig[chainId].mintFee,
+    ]
 }
